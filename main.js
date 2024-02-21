002904ff3b20cc938fbdd5362d33d596ee148fbd
jong_min <whdals0525@gmail.com>
2024-02-22 00:35:19 +0900

    jongmin-news-times

---
+++ main.js
@@ -0,0 +1,30 @@
+const API_KEY = `bb1b88cbeaa145ee9d93a45cfefc5091`
+let newsList = [];
+const getLatestNews = async() =>{
+    const url = new URL (`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    const response = await fetch(url);    
+    const data = await response.json();
+    newsList = data.articles;
+    render();
+    console.log("dddd", newsList);
+};
+
+const render=()=>{
+    const newsHTML =newsList.map(news=>`<div class="row news">
+    <div class="clo-lg-4">
+      <img
+        class="news-img-size"
+        src=${news.urlToImage}/>
+    </div>
+    <div class="clo-lg-8">
+      <h2>${news.title}</h2></h2>
+      <p>${news.description}</p>
+      <div>${news.source.name} * ${news.publishedAt}
+      </div>
+    </div>
+  </div>`
+  ).join("");
+    
+    document.getElementById("news-board").innerHTML = newsHTML
+};
+getLatestNews();
