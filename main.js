5918a9d87d1e1978784db149cfbce10c8a857017
jong_min <whdals0525@gmail.com>
2024-02-23 22:05:17 +0900

    eleven-jongmin-news-times

--- main.js
+++ main.js
@@ -1,57 +1,64 @@
-const API_KEY=`bb1b88cbeaa145ee9d93a45cfefc5091`
+const API_KEY = `bb1b88cbeaa145ee9d93a45cfefc5091`;
 let newsList = [];
-const menus = document.querySelectorAll(".menus button")
-menus.forEach(menu=>menu.addEventListener("click", (event)=>getNewsByCategory(event)))
+const menus = document.querySelectorAll(".menus button");
+menus.forEach((menu) =>
+  menu.addEventListener("click", (event) => getNewsByCategory(event))
+);
 
-let url = new URL(`https://jongmin-news-times.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`)
+let url = new URL(
+  `https://jongmin-news-times.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`
+);
 
-const getNews = async() =>{
-  try{
-    const response =await fetch(url);
+const getNews = async () => {
+  try {
+    const response = await fetch(url);
 
     const data = await response.json();
-    if(response.status===200){
-      if(data.articles.length===0){
+    if (response.status === 200) {
+      if (data.articles.length === 0) {
         throw new Error("No result for this search");
       }
       newsList = data.articles;
       render();
-    }else{
-      throw new Error(data.message)
+    } else {
+      throw new Error(data.message);
     }
-
-  } catch(error){
-    errorRender(error.message)
+  } catch (error) {
+    errorRender(error.message);
   }
 };
 
-const getLatestNews = async() =>{
-  url = new URL(`https://jongmin-news-times.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`);
+const getLatestNews = async () => {
+  url = new URL(
+    `https://jongmin-news-times.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`
+  );
   getNews();
 };
 
-const getNewsByCategory=async(event)=>{
+const getNewsByCategory = async (event) => {
   const category = event.target.textContent.toLowerCase();
-  url = new URL(`https://jongmin-news-times.netlify.app/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`
+  url = new URL(
+    `https://jongmin-news-times.netlify.app/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`
   );
   getNews();
 };
 
-const getNewsByKeyword=async()=>{
+const getNewsByKeyword = async () => {
   const keyword = document.getElementById("search-input").value;
-  url = new URL(`https://jongmin-news-times.netlify.app/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`
+  url = new URL(
+    `https://jongmin-news-times.netlify.app/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`
   );
-getNews();
+  getNews();
 };
-
-
 
 const render = () => {
-  const newsHTML = newsList.map(news=>` <div class="row news">
+  const newsHTML = newsList
+    .map(
+      (news) => ` <div class="row news">
   <div class="col-lg-4">
     <img
       class="news-img-size"
-      src=${news.urlToImage}
+      src=${news.urlToImage} onerror="this.src='https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg'"
     />
   </div>
   <div class="col-lg-8">
@@ -61,17 +68,19 @@ const render = () => {
     </p>
     <div>${news.source.name} * ${news.publishedAt}</div>
   </div>
-</div>`).join('');
+</div>`
+    )
+    .join("");
 
   document.getElementById("news-board").innerHTML = newsHTML;
 };
 
 const errorRender = (errorMessage) => {
-const errorHTML =`<div class="alert alert-danger" role="alert">
+  const errorHTML = `<div class="alert alert-danger" role="alert">
 ${errorMessage}
 </div>`;
 
-document.getElementById("news-board").innerHTML=errorHTML;
+  document.getElementById("news-board").innerHTML = errorHTML;
 };
 
 getLatestNews();
