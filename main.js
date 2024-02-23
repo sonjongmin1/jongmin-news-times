fcb0cce1845954ddf4dc8cdb0177988a02123fc2
jong_min <whdals0525@gmail.com>
2024-02-23 15:46:20 +0900

    2jongmin-news-times

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ const API_KEY=`bb1b88cbeaa145ee9d93a45cfefc5091`
 let news = [];
 
 const getLatestNews = async() =>{
-  const url = new URL(`https://jongmin-news-times.netlify.app?country=kr&apiKey=${API_KEY}`);
+  const url = new URL(`https://jongmin-news-times.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`);
   const response =await fetch(url);
   const data = await response.json();
   news = data.articles;
-  console.log("dddd", news);
+  console.log("ddd", news);
 };
 
 getLatestNews();
