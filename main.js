9265ceb6635ccf30826f25de3d47b333c0f5a051
jong_min <whdals0525@gmail.com>
2024-02-23 16:47:56 +0900

    4jongmin.news-times

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
 const API_KEY=`bb1b88cbeaa145ee9d93a45cfefc5091`
-let news = [];
+let newsList = [];
 
 const getLatestNews = async() =>{
   const url = new URL(`https://jongmin-news-times.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`);
   const response =await fetch(url);
   const data = await response.json();
-  news = data.articles;
-  console.log("ddd", news);
+  newsList = data.articles;
+  render();
+  console.log("ddd", newsList);
+};
+
+const render = () => {
+  const newsHTML = newsList.map(news=>` <div class="row news">
+  <div class="col-lg-4">
+    <img
+      class="news-img-size"
+      src=${news.urlToImage}
+    />
+  </div>
+  <div class="col-lg-8">
+    <h2>${news.title}</h2>
+    <p>
+      ${news.description}
+    </p>
+    <div>${news.source.name} * ${news.publishedAt}</div>
+  </div>
+</div>`).join('');
+
+  document.getElementById("news-board").innerHTML = newsHTML;
 };
 
 getLatestNews();
+
