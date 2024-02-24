28663367df4e12c4d74f2996e89f6f4bb6a43032
jong_min <whdals0525@gmail.com>
2024-02-24 19:20:40 +0900

    second

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
 const API_KEY = `bb1b88cbeaa145ee9d93a45cfefc5091`;
 
+let newsList = [];
+
 const getLatestNews = async () => {
   const url = new URL(
-    `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?country=us&apiKey=${API_KEY}`
+    `https://jongmin-news-times.netlify.app/top-headlines?country=us&apiKey=${API_KEY}`
   );
   const response = await fetch(url);
   const data = await response.json();
   console.log("dddd", data.articles);
+  let newsList = data.articles;
+  render();
+  console.log("dddd", newsList);
+};
+
+const render = () => {
+  const newsHTML = ``;
+
+  newsHTML = newsList.map(
+    (news) => `<div class="row news">
+  <div class="col-lg-4">
+    <img class="news-img-size" src="${news.urlToImage}" alt="" />
+  </div>
+  <div class="col-lg-8">
+    <h2>${news.title}</h2>
+    <p>${news.description}</p>
+    <div>${news.source.name} * ${news.source.publishedAt}}</div>
+  </div>
+</div>`
+  );
+
+  document.getElementById("news-board").innerHTML = newsHTML;
 };
 
 getLatestNews();
