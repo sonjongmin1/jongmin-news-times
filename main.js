7ddfc00d2c3049cf30e9fcf5a2854d65f593d3d1
jong_min <whdals0525@gmail.com>
2024-02-25 00:19:28 +0900

    third

--- main.js
+++ main.js
@@ -8,24 +8,28 @@ const getLatestNews = async () => {
   );
   const response = await fetch(url);
   const data = await response.json();
-  console.log("dddd", data.articles);
-  let newsList = data.articles;
+  newsList = data.articles;
   render();
   console.log("dddd", newsList);
 };
 
 const render = () => {
-  const newsHTML = ``;
-
-  newsHTML = newsList.map(
+  const newsHTML = newsList.map(
     (news) => `<div class="row news">
   <div class="col-lg-4">
-    <img class="news-img-size" src="${news.urlToImage}" alt="" />
+    <img
+      class="news-img-size"
+      src=${news.urlToImage}
+    />
   </div>
   <div class="col-lg-8">
     <h2>${news.title}</h2>
-    <p>${news.description}</p>
-    <div>${news.source.name} * ${news.source.publishedAt}}</div>
+    <p>
+    ${news.description}
+    </p>
+    <div>
+    ${news.source.name} * ${news.source.publishedAt}
+    </div>
   </div>
 </div>`
   );
