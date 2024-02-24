110fa0d1d69d02c84e86e064dd76395eed22cc63
jong_min <whdals0525@gmail.com>
2024-02-25 02:44:09 +0900

    seven

--- main.js
+++ main.js
@@ -10,8 +10,16 @@ let url = new URL(
   `https://jongmin-news-times.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`
 );
 
+let totalResults = 0;
+let page = 1;
+const pageSize = 10;
+const groupSize = 5;
+
 const getNews = async () => {
   try {
+    url.searchParams.set("page", page); // => &page=page
+    url.searchParams.set("pageSize", pageSize);
+
     const response = await fetch(url);
 
     const data = await response.json();
@@ -20,7 +28,9 @@ const getNews = async () => {
         throw new Error("No result for this search");
       }
       newsList = data.articles;
+      totalResults = data.totalResults;
       render();
+      paginationRender();
     } else {
       throw new Error(data.message);
     }
@@ -87,6 +97,42 @@ const errorRender = (errorMessage) => {
     </div>`;
 
   document.getElementById("news-board").innerHTML = errorHTML;
+};
+
+const paginationRender = () => {
+  //totalResults
+  //page
+  //pageSize
+  //groupSize
+  //totalpages
+  const totalPages = Math.ceil(totalResults / pageSize);
+  //pageGroup
+  const pageGroup = Math.ceil(page / groupSize);
+  //lastPage
+  const lastPage = pageGroup * groupSize;
+  //마지막 페이지그룹이 그룹사이즈보다 작다? lastpage = totalpage
+  if (lastPage > totalPages) {
+    lastPage = totalPages;
+  }
+
+  const firstPage =
+    lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1);
+
+  let paginationHTML = ``;
+
+  for (let i = firstPage; i <= lastPage; i++) {
+    paginationHTML += `<li class="page-item ${
+      i === page ? "active" : ""
+    }" onclick="moveToPage(${i})"><a class="page-link" >${i}</a></li>`;
+  }
+
+  document.querySelector(".pagination").innerHTML = paginationHTML;
+};
+
+const moveToPage = (pageNum) => {
+  console.log("movetopage", pageNum);
+  page = pageNum;
+  getNews();
 };
 
 getLatestNews();
