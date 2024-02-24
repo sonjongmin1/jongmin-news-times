b893c2eb3646a76e7e2202e5b862906399fd954c
jong_min <whdals0525@gmail.com>
2024-02-24 15:24:31 +0900

    first

--- main.js
+++ main.js
@@ -1,86 +1,12 @@
 const API_KEY = `bb1b88cbeaa145ee9d93a45cfefc5091`;
-let newsList = [];
-const menus = document.querySelectorAll(".menus button");
-menus.forEach((menu) =>
-  menu.addEventListener("click", (event) => getNewsByCategory(event))
-);
-
-let url = new URL(
-  `https://jongmin-news-times.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`
-);
-
-const getNews = async () => {
-  try {
-    const response = await fetch(url);
-
-    const data = await response.json();
-    if (response.status === 200) {
-      if (data.articles.length === 0) {
-        throw new Error("No result for this search");
-      }
-      newsList = data.articles;
-      render();
-    } else {
-      throw new Error(data.message);
-    }
-  } catch (error) {
-    errorRender(error.message);
-  }
-};
 
 const getLatestNews = async () => {
-  url = new URL(
-    `https://jongmin-news-times.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`
+  const url = new URL(
+    `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?country=us&apiKey=${API_KEY}`
   );
-  getNews();
-};
-
-const getNewsByCategory = async (event) => {
-  const category = event.target.textContent.toLowerCase();
-  url = new URL(
-    `https://jongmin-news-times.netlify.app/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`
-  );
-  getNews();
-};
-
-const getNewsByKeyword = async () => {
-  const keyword = document.getElementById("search-input").value;
-  url = new URL(
-    `https://jongmin-news-times.netlify.app/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`
-  );
-  getNews();
-};
-
-const render = () => {
-  const newsHTML = newsList
-    .map(
-      (news) => ` <div class="row news">
-  <div class="col-lg-4">
-    <img
-      class="news-img-size"
-      src=${news.urlToImage} onerror="this.src='https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg'"
-    />
-  </div>
-  <div class="col-lg-8">
-    <h2>${news.title}</h2>
-    <p>
-      ${news.description}
-    </p>
-    <div>${news.source.name} * ${news.publishedAt}</div>
-  </div>
-</div>`
-    )
-    .join("");
-
-  document.getElementById("news-board").innerHTML = newsHTML;
-};
-
-const errorRender = (errorMessage) => {
-  const errorHTML = `<div class="alert alert-danger" role="alert">
-${errorMessage}
-</div>`;
-
-  document.getElementById("news-board").innerHTML = errorHTML;
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log("dddd", data.articles);
 };
 
 getLatestNews();
