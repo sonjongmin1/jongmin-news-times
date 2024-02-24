1b5b0ec483aa931dc0d80bbfd0d39feba14fd79a
jong_min <whdals0525@gmail.com>
2024-02-25 01:18:21 +0900

    four

--- main.js
+++ main.js
@@ -1,21 +1,52 @@
 const API_KEY = `bb1b88cbeaa145ee9d93a45cfefc5091`;
 
 let newsList = [];
+const menus = document.querySelectorAll(".menus button");
+menus.forEach((menu) =>
+  menu.addEventListener("click", (event) => getNewsByCategory(event))
+);
 
-const getLatestNews = async () => {
-  const url = new URL(
-    `https://jongmin-news-times.netlify.app/top-headlines?country=us&apiKey=${API_KEY}`
-  );
+let url = new URL(
+  `https://jongmin-news-times.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`
+);
+
+const getNews = async () => {
   const response = await fetch(url);
   const data = await response.json();
   newsList = data.articles;
   render();
-  console.log("dddd", newsList);
+};
+
+const getLatestNews = async () => {
+  url = new URL(
+    `https://jongmin-news-times.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`
+  );
+
+  getNews();
+};
+
+const getNewsByCategory = async (event) => {
+  const category = event.target.textContent.toLowerCase();
+  url = new URL(
+    `https://jongmin-news-times.netlify.app/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`
+  );
+
+  getNews();
+};
+
+const getNewsByKeyword = async () => {
+  const keyword = document.getElementById("search-input").value;
+  url = new URL(
+    `https://jongmin-news-times.netlify.app/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`
+  );
+
+  getNews();
 };
 
 const render = () => {
-  const newsHTML = newsList.map(
-    (news) => `<div class="row news">
+  const newsHTML = newsList
+    .map(
+      (news) => `<div class="row news">
   <div class="col-lg-4">
     <img
       class="news-img-size"
@@ -32,7 +63,8 @@ const render = () => {
     </div>
   </div>
 </div>`
-  );
+    )
+    .join("");
 
   document.getElementById("news-board").innerHTML = newsHTML;
 };
